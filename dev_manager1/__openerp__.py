e378c8dd8648e4488e834b1afb175dad2bad540a
AlvaroFC99 <50100115+AlvaroFC99@users.noreply.github.com>
2020-02-19 17:47:47 +0100

    Add files via upload

---
+++ dev_manager1/__openerp__.py
@@ -0,0 +1,12 @@
+{ 
+    'name': 'Developments Manager',
+    'version':'1.1',
+    'description':"""modulo gestion de solicitudes de desarrollo""", 
+    'author': 'EEP - Alvaro Fernandez', 
+    'depends': ['base'], 
+    'application': True,
+    'data' : ['views/devman_views1.xml',
+              'views/report_devman_task_hours.xml',
+              'dev_manager_report.xml'],
+ 
+}
